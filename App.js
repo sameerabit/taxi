90d1aadabd8eb1fca0ad92e45d862d5905f0788b
sameera <sameera@sameera>
2017-07-09 17:24:19 +0530

    map added

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <MapView
+   initialRegion={{
+     latitude: 37.78825,
+     longitude: -122.4324,
+     latitudeDelta: 0.0922,
+     longitudeDelta: 0.0421,
+   }}
+ />
     );
   }
 }
